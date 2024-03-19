f8b1bc641eeb014200f19a18f6de465979182fe5
Anton <53652055+from-lamer-to-programmer@users.noreply.github.com>
2024-03-19 21:29:18 +0300

    profile update

--- anifigure/users/views.py
+++ anifigure/users/views.py
@@ -1,44 +1,33 @@
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
-from users.forms import LoginUserForm
+from users.forms import LoginUserForm, RegisterForm
 
 
 class LoginUser(LoginView):
-    form_class = AuthenticationForm
+    form_class = LoginUserForm
     template_name = 'users/login.html'
     extra_context = {'title':'Авторизация'}
 
     # def get_success_url(self):
     #     return reverse_lazy('main')
 
-# def login_user(request):
-#     if request.method == 'POST':
-#         form = LoginUserForm(request.POST)
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             user = authenticate(request, username=cd['username'], password=cd['password'])
-#             if user and user.is_active:
-#                 login(request, user)
-                # return HttpResponseRedirect(reverse('main'))
-    #
-    # else:
-    #     form = LoginUserForm()
-    #
-    # return render(request, 'users/login.html', {'form': form})
-
-
 def registration_user(request):
-    pass
-
-    return render(request, 'users/login.html', {'form': form})
+    if request.method == 'GET':
+        form = RegisterForm()
+        return render(request, 'users/register.html', { 'form': form})
 
 
 def logout_user(request):
-    return HttpResponseRedirect(reverse('users:login'))
+
+    form = RegisterForm()
+
+    return render(request, 'users/register.html', { 'form': form})
+
 
 
 def profile_user(request):
