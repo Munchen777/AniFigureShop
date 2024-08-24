5798412ec0bb4ff9525e7dd245d8c9cd34930635
Георгий Исак <isak.georgiy@mail.ru>
2024-08-24 18:53:00 +0300

    add users urls routing

--- anifigure/users/urls.py
+++ anifigure/users/urls.py
@@ -1,15 +1,19 @@
 from django.urls import path
-from . import views
+
+
+from .views import RegisterAPIView, LoginAPIView
+
 
 app_name = 'users'
 
+
 urlpatterns = [
-    path("login/", views.LoginUser.as_view(), name="login"),
-    path("logout/", views.logout_user, name="logout"),
-    path("registration/", views.RegisterUser.as_view(), name="registration"),
-    path("profile/", views.profile_user, name="profile"),
-
-    path("cart/", views.cart_items, name="cart"),
-
+    path("register/", RegisterAPIView.as_view(), name="register"),
+    path("login/", LoginAPIView.as_view(), name="login"),
+    # path("login/", views.LoginUser.as_view(), name="login"),
+    # path("logout/", views.logout_user, name="logout"),
+    # path("register/", views.RegisterUser.as_view(), name="registration"),
+    # path("profile/", views.profile_user, name="profile"),
+    # path("cart/", views.cart_items, name="cart"),
+    
 ]
-
