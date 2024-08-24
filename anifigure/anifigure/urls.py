f313779a2fdab27e242af877447379dac849eae7
Георгий Исак <isak.georgiy@mail.ru>
2024-08-24 18:35:33 +0300

    implement project urls

--- anifigure/anifigure/urls.py
+++ anifigure/anifigure/urls.py
@@ -1,16 +1,30 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from rest_framework_simplejwt.views import TokenRefreshView
+from drf_spectacular.views import (SpectacularAPIView,
+                                   SpectacularRedocView,
+                                   SpectacularSwaggerView)
 
-from anifigure import settings
+
+from api.views import MyTokenObtainPairView
 from base.views import page_not_found
 
+
 urlpatterns = [
-    path("", include("base.urls")),
+    # path("", include("base.urls", namespace="base")),
+    path("api/", include("api.urls")),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path("users/", include("users.urls", namespace="users")),
     path("admin/", admin.site.urls),
-]
 
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
