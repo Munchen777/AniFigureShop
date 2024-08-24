79fdfc82149b594fb9e4429c5195db3d3b55dfac
Георгий Исак <isak.georgiy@mail.ru>
2024-08-24 18:48:57 +0300

    add models in admin

---
+++ anifigure/products/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+
+
+from .models import Product, ProductImage, Category
+
+
+class ProductInline(admin.TabularInline):
+    model = ProductImage
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["pk", "name", "description", "discount", "price"]
+    inlines = [
+        ProductInline
+    ]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "parent_category", "description", "slug"]
+
+
+
