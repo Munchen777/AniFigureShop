b413793cdf423382e665708539d891ca23f34d37
Георгий Исак <isak.georgiy@mail.ru>
2024-09-01 17:14:06 +0300

    delete unused styles

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,42 +5,6 @@ import { Component } from "react";
 class Footer extends Component {
     render () {
         return (
-            // <Container
-            // className="bg-[#8dc2cb] mx- sm:mx-auto max-w-7xl"
-            // component="div"
-            // fixed="false"
-            // maxWidth="xl"
-
-            // >
-            //     <Box
-            //     sx={{
-            //     display: 'flex',
-            //     flexDirection: 'column',
-            //     justifyContent:'space-between'
-            //     }}
-            //     >
-            //         <Stack
-            //         spacing="48px"
-            //         >
-            //             <Link
-            //             >
-            //                 <Typography
-            //                 variant="h6"
-            //                 >Отзывы
-            //                 </Typography>
-            //             </Link>
-            //             <Link
-            //             >
-            //                 <Typography
-            //                 variant="h6"
-            //                 >
-            //                     Контактная информация
-            //                 </Typography>
-            //             </Link>
-            //         </Stack>
-            //     </Box>
-            // </Container>
-
             <div className="md:mx-auto md:container bg-slate-400 rounded-md justify-center sm:mx-12">
                 <div className="flex justify-between">
                     <div className="flex flex-col gap-y-8 md:px-3">
