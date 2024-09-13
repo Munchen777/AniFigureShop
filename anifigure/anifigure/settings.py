1b627037a2363f53a94fbf809c1b19ce850c7d7f
Георгий Исак <isak.georgiy@mail.ru>
2024-09-13 18:04:40 +0300

    add corsheaders

--- anifigure/anifigure/settings.py
+++ anifigure/anifigure/settings.py
@@ -37,12 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'django_extensions',
+    'corsheaders',
 
     'base.apps.BaseConfig',
     'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +55,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'anifigure.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
+CORS_ALLOW_CREDENTIALS = True
 
 TEMPLATES = [
     {
