487af6fe156337a26092607bcd5069ba24f6c8ba
munchen777.eth <isak.georgiy@mail.ru>
2024-09-26 20:45:55 +0300

    order models

--- anifigure/order/models.py
+++ anifigure/order/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
 
 from products.models import Product
@@ -8,7 +9,7 @@ class Order(models.Model):
     promocode = models.CharField(max_length=40)
     delivery_address = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=None)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_DEFAULT, default=None)
     products = models.ManyToManyField(Product, related_name='orders')
 
     class Meta:
