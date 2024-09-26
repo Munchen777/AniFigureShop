44c1cef8cd358e34c1bb6b431839ee7009a0d7d3
munchen777.eth <isak.georgiy@mail.ru>
2024-09-26 20:49:13 +0300

    create root api

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+export const ROOT_API = "http://127.0.0.1:8000/"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
