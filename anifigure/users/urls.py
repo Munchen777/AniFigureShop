81473fa881ff828b4ef82538e2438ff2721bf394
munchen777.eth <isak.georgiy@mail.ru>
2024-10-04 19:01:40 +0300

    add login api and login template

--- anifigure/users/urls.py
+++ anifigure/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 
-from .views import RegisterAPIView, LoginAPIView
+from .views import RegisterAPIView, LoginAPIView, LoginTemplateView
 
 
 app_name = 'users'
@@ -9,7 +9,8 @@ app_name = 'users'
 
 urlpatterns = [
     path("register/", RegisterAPIView.as_view(), name="register"),
-    path("login/", LoginAPIView.as_view(), name="login"),
+    path("api/login/", LoginAPIView.as_view(), name="login"),
+    path("login/", LoginTemplateView.as_view(), name="login-template"),
     # path("login/", views.LoginUser.as_view(), name="login"),
     # path("logout/", views.logout_user, name="logout"),
     # path("register/", views.RegisterUser.as_view(), name="registration"),
