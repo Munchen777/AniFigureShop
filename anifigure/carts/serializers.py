b148da3c05619abd628e113c7a6d94d9556aa8cb
munchen777.eth <isak.georgiy@mail.ru>
2025-01-18 18:03:18 +0300

    update cart serializer

--- anifigure/carts/serializers.py
+++ anifigure/carts/serializers.py
@@ -1,11 +1,8 @@
 from rest_framework import serializers
 
-
-from products.models import Product
 from products.serializers import ProductSerializer
 from orders.models import OrderItem
-from users.serializers import UserSerializer
-from .models import Cart, CartItem
+from .models import Cart
 
 
 class OrderedItemSerializer(serializers.ModelSerializer):
@@ -14,25 +11,12 @@ class OrderedItemSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class CartItemSerializer(serializers.ModelSerializer):
-    product = ProductSerializer() # write_only=True
-
-    class Meta:
-        model = CartItem
-        fields = ["cart", "product", "updated_at", "created_at", "quantity"]
-        extra_kwargs = {
-            "cart": {"write_only": True, "required": False},
-            "product": {"write_only": True},
-        }
-
-
 class CartSerializer(serializers.ModelSerializer):
-    items = CartItemSerializer(many=True)
-    user = UserSerializer()
+    product = ProductSerializer()
 
     class Meta:
         model = Cart
-        fields = ['session_key', 'user', "quantity", "created_at"]
-        extra_kwargs = {
-            "user": {"required": False, "write_only": True},
-        }
+        fields = [
+            "product",
+            "quantity",
+        ]
