c449e82111101b59e14bd13ba2099af8296c6faa
munchen777.eth <isak.georgiy@mail.ru>
2025-01-29 11:29:04 +0300

    add custom email backend

--- anifigure/users/backends.py
+++ anifigure/users/backends.py
@@ -1,5 +1,9 @@
+import ssl
+
+from django.core.mail.backends.smtp import EmailBackend as SMTPBackend
 from django.contrib.auth.backends import ModelBackend, get_user_model
 from django.core.exceptions import MultipleObjectsReturned
+from django.utils.functional import cached_property
 from django.db.models import Q
 
 
@@ -26,3 +30,17 @@ class UserModelBackend(ModelBackend):
         else:
             if user.check_password(password) and self.user_can_authenticate(user):
                 return user
+
+
+class EmailBackend(SMTPBackend):
+    @cached_property
+    def ssl_context(self):
+        if self.ssl_certfile or self.ssl_keyfile:
+            ssl_context = ssl.SSLContext(protocol=ssl.PROTOCOL_TLS_CLIENT)
+            ssl_context.load_cert_chain(self.ssl_certfile, self.ssl_keyfile)
+            return ssl_context
+        
+        ssl_context = ssl.create_default_context()
+        ssl_context.check_hostname = False
+        ssl_context.verify_mode = ssl.CERT_NONE
+        return ssl_context
