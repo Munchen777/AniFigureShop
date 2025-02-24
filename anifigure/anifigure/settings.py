3e2f4d09f03f73a20f9acaa30c4ee8b5de9d453f
munchen777.eth <isak.georgiy@mail.ru>
2025-02-24 11:02:08 +0300

    add env variables and config for email, add banners app

--- anifigure/anifigure/settings.py
+++ anifigure/anifigure/settings.py
@@ -9,9 +9,13 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os
 
+from dotenv import load_dotenv
 from pathlib import Path
 from datetime import timedelta
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +53,7 @@ INSTALLED_APPS = [
     "carts.apps.CartsConfig",
     "products.apps.ProductsConfig",
     "orders.apps.OrdersConfig",
+    "banners.apps.BannersConfig",
     
     # For REST
     "corsheaders",
@@ -174,6 +179,18 @@ AUTH_USER_MODEL = "users.CustomUser"
 CART_SESSION_ID = "cart"
 
 ####################################
+# Email variables
+####################################
+EMAIL_BACKEND = "users.backends.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_PORT = 587
+EMAIL_HOST_USER = str(os.getenv("EMAIL_USER"))
+EMAIL_HOST_PASSWORD = str(os.getenv("EMAIL_PASSWORD"))
+
+####################################
 # SIMPLE JWT
 ####################################
 SIMPLE_JWT = {
@@ -187,13 +204,16 @@ SIMPLE_JWT = {
 ####################################
 # CORS HEADERS
 ####################################
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+#     "http://localhost:8000",
+#     "http://127.0.0.1:8000",
+# ]
 CORS_ALLOW_CREDENTIALS = True
 # CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 ####################################
 # REST FRAMEWORK
