395d2f3c30fe3b4b0b34871ca4ae901066390f10
munchen777.eth <isak.georgiy@mail.ru>
2025-02-24 14:16:45 +0300

    add header, footer and hero components

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,114 +1,193 @@
-import React, { Component, useState } from "react";
+import React, { Component, useContext, useState } from "react";
 
-import AppBar from '@mui/material/AppBar'
-import MenuIcon from '@mui/icons-material/Menu';
-import Typography from "@mui/material/Typography";
-import { IconButton, Toolbar } from "@mui/material";
-import {Link} from 'react-router-dom'
-import { GiShoppingCart } from "react-icons/gi";
-
-
-// function ThemeSwitcher() {
-//   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-
-//   const theme = React.useMemo(
-//     () =>
-//       createTheme({
-//         palette: {
-//           mode: prefersDarkMode ? 'dark' : 'light',
-//         },
-//       }),
-//     [prefersDarkMode],
-//   );
-
-//   return (
-//     <ThemeProvider theme={theme}>
-//       <CssBaseline />
-//     </ThemeProvider>
-//   );
-// }
-
-
-// function MenuPopup() {
-//   const [opened, setOpened] = useState(0);
-
-//   function openPopup() {
-//     setOpened(1);
-//   }
-// };
-
+import { Link } from "react-router-dom";
+import AuthContext from "../contexts/AuthContext";
+import { PiShoppingCartDuotone } from "react-icons/pi";
+import { VscAccount } from "react-icons/vsc";
 
 export default function Header() {
+  let { user, loginUser, logoutUser } = useContext(AuthContext);
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isDropdownOpen, setisDropdownOpen] = useState(false);
+  const [cartItemsCount, setCartItemsCount] = useState(0);
+
   return (
-    <AppBar>
-    <Toolbar sx={{display: "flex"}}>
-      <IconButton
-        size="large"
-        edge="start"
-        color="inherit"
-        aria-label="open drawer"
-        sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-      </IconButton>
-      <Typography
-        variant="h6"
-        noWrap
-        component="div"
-        sx={{ display: { xs: 'none', sm: 'block' } }}
-        >
-        Anifigure Shop
-      </Typography>
-      <Link to="/login" />
-      <GiShoppingCart />
-    </Toolbar>
-  </AppBar>
-  )
+    <header className="bg-white">
+      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+        <div className="flex lg:flex-1">
+          <a href="#">Логотип</a>
+        </div>
+
+        {/* Кнопка для открытия меню на мобильных устройствах */}
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          </button>
+        </div>
+
+        {/* Меню для крупных экранов */}
+        <div className="hidden lg:flex lg:gap-x-12 items-center">
+          <a href="#" className="text-sm font-semibold text-gray-900">
+            Каталог
+          </a>
+          {/* <a href="#" className="text-sm font-semibold text-gray-900">Marketplace</a> */}
+          <a href="#" className="text-sm font-semibold text-gray-900">
+            О компании
+          </a>
+        </div>
+
+        <div className="hidden lg:flex lg:flex-1  relative lg:justify-evenly items-center">
+          <div className="flex relative">
+            <Link to={"/cart"}>
+              <PiShoppingCartDuotone className="w-5 h-5" />
+              {cartItemsCount > 0 && (
+                <span className="absolute -bottom-1 -right-1 bg-red-500 text-white text-xs rounded-full h-4 w-4 flex items-center justify-center">
+                  {cartItemsCount}
+                </span>
+              )}
+            </Link>
+          </div>
+          {/* <div className="flex">
+            <Link
+              to={"/cart"}
+            >
+              <PiShoppingCartDuotone className="w-5 h-5" />
+            </Link>
+          </div> */}
+          {user ? (
+            <div className="relative group">
+              <button
+                className="flex items-center p-2 rounded-full hover:bg-gray-100 transition-colors"
+                onMouseEnter={() => setisDropdownOpen(true)}
+                onMouseLeave={() => setisDropdownOpen(false)}
+              >
+                <VscAccount className="w-6 h-6"/>
+              </button>
+
+              {/* Выпадающий блок */}
+              {isDropdownOpen && (
+                <div
+                  className="absolute right-0 w-48 bg-white text-black shadow-md rounded-md z-10 dark:bg-zinc-50"
+                  onMouseEnter={() => setisDropdownOpen(true)}
+                  onMouseLeave={() => setisDropdownOpen(false)}
+                >
+                  <Link
+                    to={"/profile"}
+                    className="block w-full text-left px-4 py-2 hover:bg-gray-100"
+                  >
+                    Профиль
+                  </Link>
+                  <Link
+                    onClick={logoutUser}
+                    className="block w-full text-left px-4 py-2 hover:bg-gray-100"
+                  >
+                    Выйти
+                  </Link>
+                </div>
+              )}
+            </div>
+          ) : (
+            <div className="flex items-center space-x-4">
+              <Link
+                to="/login"
+                className="text-sm font-semibold text-gray-900 hover:text-gray-700 transition-colors"
+              >
+                Login
+              </Link>
+              <span className="h-4 w-px bg-gray-300"></span>
+              <Link
+                to="/register"
+                className="text-sm font-semibold text-gray-900 hover:text-gray-700 transition-colors"
+              >
+                Register
+              </Link>
+            </div>
+            // <Link
+            // to="/login"
+            // className="text-sm font-semibold text-gray-900"
+            // >
+            //   Login
+            // </Link>
+          )}
+        </div>
+      </nav>
+
+      {/* Мобильное меню */}
+      {isMobileMenuOpen && (
+        <div className="lg:hidden">
+          <div className="space-y-2 p-4">
+            <a href="#" className="block text-sm font-semibold text-gray-900">
+              Каталог
+            </a>
+            {/* <a href="#" className="block text-sm font-semibold text-gray-900">Marketplace</a> */}
+            <a href="#" className="block text-sm font-semibold text-gray-900">
+              О компании
+            </a>
+            <hr className="my-2" />
+            {user ? (
+              <Link
+                className="flex items-center w-full text-sm font-semibold text-gray-900 hover:bg-gray-100 rounded-md p-2"
+                onClick={() => setisDropdownOpen(!isDropdownOpen)}
+                // onMouseEnter={() => setisDropdownOpen(true)}
+                // onMouseLeave={() => setisDropdownOpen(false)}
+              >
+                {user.username || user.email}
+              </Link>
+            ) : (
+              <div className="flex flex-col space-y-2">
+                <Link
+                  to="/login"
+                  className="block w-full text-sm font-semibold text-gray-900 hover:bg-gray-100 rounded-md transition-colors"
+                >
+                  Login
+                </Link>
+                <Link
+                  to="/register"
+                  className="block w-full text-sm font-semibold text-gray-900 hover:bg-gray-100 rounded-md transition-colors"
+                >
+                  Register
+                </Link>
+              </div>
+            )}
+
+            {isDropdownOpen && (
+              <div
+                onMouseEnter={() => setisDropdownOpen(true)}
+                onMouseLeave={() => setisDropdownOpen(false)}
+                className="relative"
+              >
+                <button className="block w-full text-left px-4 py-2 hover:bg-gray-100">
+                  Профиль
+                </button>
+                <button
+                  className="block w-full text-left px-4 py-2 hover:bg-gray-100"
+                  onClick={logoutUser}
+                >
+                  Выйти
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </header>
+  );
 }
-
-
-// class Header extends Component {
-//     render() {
-//         return (
-          // <header className="bg-while">
-          //   <nav className="mx-auto flex max-w items-center justify-between p-6 lg:px-12" aria-label="Global">
-
-          //     <div className="flex lg:flex-1">
-          //       <a href="#">
-          //         <img className="h-8 w-auto" src="#"></img>
-          //       </a>
-          //     </div>
-
-          //     <div>
-
-          //     </div>
-
-          //   </nav>
-          // </header>
-  
-//           <AppBar>
-//             <Toolbar>
-//               <IconButton
-//                 size="large"
-//                 edge="start"
-//                 color="inherit"
-//                 aria-label="open drawer"
-//                 sx={{ mr: 2 }}
-//                 >
-//                   <MenuIcon />
-//               </IconButton>
-//               <Typography
-//                 variant="h6"
-//                 noWrap
-//                 component="div"
-//                 sx={{ display: { xs: 'none', sm: 'block' } }}
-//                 >
-//                 Anifigure Shop
-//               </Typography>
-//             </Toolbar>
-//           </AppBar>  
-//         )
-//     }
-// };
-
-// export default Header;
---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,55 @@
+export default function Hero() {
+  return (
+    <>
+      <section className="mx-auto container bg-zinc-200 py-20 mt-10 rounded-2xl drop-shadow-2xl">
+        <div className="container mx-auto px-6 text-center lg:text-left">
+          <h1 className="text-4xl font-bold text-black sm:text-5xl lg:text-6xl">
+            Digital IT
+          </h1>
+          <p className="mt-4 text-lg text-gray-800 font-semibold sm:max-w-xl lg:max-w-2xl">
+            Приветствуем вас в нашем магазине цифровых товаров!
+          </p>
+        </div>
+      </section>
+      <div className="mx-auto lg:mt-14 h-10 sm:mt-6 md:mt-9 sm:my-4 my-3">
+        <svg
+          id="patternId"
+          width="100%"
+          height="100%"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <pattern
+              id="a"
+              patternUnits="userSpaceOnUse"
+              width="78.141"
+              height="46"
+              patternTransform="scale(4) rotate(135)"
+            >
+              <rect
+                x="0"
+                y="0"
+                width="100%"
+                height="100%"
+                fill="hsla(0,0%,20.8%,1)"
+              />
+              <path
+                d="M69.212 40H46.118L34.57 20 46.118 0h23.094l11.547 20zM57.665 60H34.57L23.023 40 34.57 20h23.095l11.547 20zm0-40H34.57L23.023 0 34.57-20h23.095L69.212 0zM34.57 60H11.476L-.07 40l11.547-20h23.095l11.547 20zm0-40H11.476L-.07 0l11.547-20h23.095L46.118 0zM23.023 40H-.07l-11.547-20L-.07 0h23.094L34.57 20z"
+                transform="translate(4.5,0)"
+                stroke-width="1"
+                stroke="hsla(0,0%,85.1%,1)"
+                fill="none"
+              />
+            </pattern>
+          </defs>
+          <rect
+            width="800%"
+            height="800%"
+            transform="translate(-488,-180)"
+            fill="url(#a)"
+          />
+        </svg>
+      </div>
+    </>
+  );
+}
