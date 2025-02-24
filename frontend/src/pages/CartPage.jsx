fa513d56111424df085a493302f4eb0b3edc6971
munchen777.eth <isak.georgiy@mail.ru>
2025-02-24 14:14:15 +0300

    fix: add cart, main pages

---
+++ frontend/src/pages/CartPage.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import CartComponent from "../components/CartComponent";
+
+export default function CartPage() {
+  return (
+    <>
+      <Header />
+      <main>
+        <CartComponent />
+      </main>
+      <Footer />
+    </>
+  );
+}
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,18 +1,20 @@
 import Header from "../components/Header";
 import MainPage from "../components/MainPage";
 import Footer from "../components/Footer";
-
+import { AuthProvider } from "../contexts/AuthContext";
+import Hero from "../components/Hero";
 
 export default function HomePage(params) {
   return (
     <>
-      <Header />
-      <main className="bg-red-200">
-        <MainPage />
-      </main>
-      <footer>
+      <AuthProvider>
+        <Header />
+        <Hero />
+        {/* <main>
+          <MainPage />
+        </main> */}
         <Footer />
-      </footer>
+      </AuthProvider>
     </>
   );
 }
