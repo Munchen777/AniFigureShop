b8a72e61dd31d59c1d2dc6b4bad2e608f8fa6a32
munchen777.eth <isak.georgiy@mail.ru>
2025-02-24 14:07:03 +0300

    fix: change password reset routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import PrivateRoute from "./pages/PrivateRoute";
 import { AuthProvider } from "./contexts/AuthContext";
 import { CartProvider } from "./contexts/CartContext";
-import LoginForm from "./components/LoginForm";
-import RegisterForm from "./pages/RegisterForm";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ResetPasswordDone from "./components/ResetPasswordDone";
 import ResetPasswordSuccess from "./components/ResetPasswordSuccess";
 import ResetPassword from "./components/ResetPassword";
 import PasswordResetForm from "./components/PasswordResetForm";
@@ -22,8 +22,9 @@ export default function App() {
               {/* Другие защищенные маршруты можно добавлять внутри PrivateRoute */}
             </Route>
 
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/register" element={<RegisterForm />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/reset-password-done" element={<ResetPasswordDone />} />
             <Route path="/reset-password-success" element={<ResetPasswordSuccess />} />
             <Route path="/reset-password" element={<ResetPassword />} />
             <Route path="/users/api/v1/reset-password-confirm/:uidb64/:token/" element={<PasswordResetForm />} />
