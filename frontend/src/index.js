52ce4c184439ef4735740b56146804769b86b74d
munchen777.eth <isak.georgiy@mail.ru>
2025-02-28 19:53:54 +0300

    change api host

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-export const ROOT_API = "http://127.0.0.1:8000"
+export const ROOT_API = "http://localhost:8000"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
